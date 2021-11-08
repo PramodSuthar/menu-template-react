78902c872bf776029059f4197c2791fbb2c7afb4
Pramod Suthar <pramodsuthar@mail.com>
2021-11-07 21:52:44 -0400

    final commit

---
+++ src/Menu.js
@@ -0,0 +1,20 @@
+import React from 'react';
+//will hold the menu items                                                                                                                                                                                                                                                     
+const Menu = ({ items }) => {
+    return <div className="section-center">
+        {items.map((menuItem) => {
+            const { id, title, img, desc, price } = menuItem;
+            return (<article key={id} className="menu-item">
+                <img src={img} alt={title} className='photo' />
+                <div className="item-info">
+                    <header>
+                        <h4 className="price">${price}</h4>
+                    </header>
+                    <p className='item-text'>{desc}</p>
+                </div>
+            </article>)
+        })}
+    </div>;
+};
+
+export default Menu;
